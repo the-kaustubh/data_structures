a63b130372a355c6404759739408183245f75657
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2018-08-28 23:06:13 +0530

    Implement delete

--- LinkedLists/circularDoublyLinkedList.c
+++ LinkedLists/circularDoublyLinkedList.c
@@ -1,7 +1,7 @@
 #include "circularDoublyLinkedList.h"
 
 Node insertAtStart(Node start, int data) {
-  Node new = malloc(block), last = start->prev;
+  Node new = malloc(CDLL_BLOCK_SIZE), last = start->prev;
   new->data = data;
    last->next =   new;
     new->prev =  last;
@@ -11,7 +11,7 @@ Node insertAtStart(Node start, int data) {
 }
 
 Node insertAtEnd(Node start, int data) {
-  Node new = malloc(block), last = start->prev;
+  Node new = malloc(CDLL_BLOCK_SIZE), last = start->prev;
   new->data = data;
   last->next = new;
   new->prev = last;
@@ -21,7 +21,7 @@ Node insertAtEnd(Node start, int data) {
 }
 
 Node insertAtPosition(Node start, int data, int pos) {
-  Node new = malloc(block), temp1 = start, temp2;
+  Node new = malloc(CDLL_BLOCK_SIZE), temp1 = start, temp2;
   new->data = data;
   for(int i = 1; i < pos; i++) temp1 = temp1->next;
   temp2 = temp1->next;
@@ -29,5 +29,38 @@ Node insertAtPosition(Node start, int data, int pos) {
   new->prev = temp1;
   new->next = temp2;
   temp2->prev = new;
+  return start;
+}
+
+
+Node deleteKeyDLL(Node start, int key) {
+  /*(
+    delete a `Node` having data as `key`
+      if key is in next Node, then free the next Node
+      but put later Node in `next`
+  )*/
+  Node pre, next;
+  pre = start;
+  for(;pre->next != NULL; pre = pre->next) {
+    if(pre->next->data == key) {
+      next = pre->next->next;
+      free(pre->next);
+      pre->next = next;
+      next->prev = pre;
+      return start;
+    }
+  }
+}
+
+Node deletePositionDLL(Node start, int pos) {
+  /*
+    delete a Node from Given position.
+  */
+  Node new = malloc(DLL_BLOCK_SIZE), temp1 = start, temp2;
+  for(int i = 1; i < pos; i++) temp1 = temp1->next;
+  temp2 = temp1->next->next;
+  free(temp1->next);
+  temp1->next = temp2;
+  temp2->prev = temp1;
   return start;
 }
--- LinkedLists/doublyLinkedList.c
+++ LinkedLists/doublyLinkedList.c
@@ -1,15 +1,15 @@
-  #include <stdio.h>
-  #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node {
   int data;
   struct node * next;
   struct node * prev;
 }*Node;
-long unsigned int block = sizeof(Node);
+long unsigned int DLL_BLOCK_SIZE = sizeof(Node);
 
 Node insertAtStart(Node start, int data) {
-  Node new = malloc(block);
+  Node new = malloc(DLL_BLOCK_SIZE);
   new->data = data;
     new->next   = start;
   start->prev   =   new;
@@ -17,7 +17,7 @@ Node insertAtStart(Node start, int data) {
 }
 
 Node insertAtEnd(Node start, int data) {
-  Node new = malloc(block), temp = start;
+  Node new = malloc(DLL_BLOCK_SIZE), temp = start;
   new->data = data;
   new->next = NULL;
   while(temp->next != NULL) temp = temp->next;
@@ -27,7 +27,7 @@ Node insertAtEnd(Node start, int data) {
 }
 
 Node insertAtPosition(Node start, int data, int pos) {
-  Node new = malloc(block), temp1 = start, temp2;
+  Node new = malloc(DLL_BLOCK_SIZE), temp1 = start, temp2;
   new->data = data;
   for(int i = 1; i < pos; i++) temp1 = temp1->next;
   temp2 = temp1->next;
@@ -37,3 +37,35 @@ Node insertAtPosition(Node start, int data, int pos) {
   temp2->prev = new;
   return start;
 }
+
+
+Node deleteKeyDLL(Node start, int key) {
+  /*(
+    delete a `Node` having data as `key`
+      if key is in next Node, then free the next Node
+      but put later Node in `next`
+  )*/
+  Node pre, next;
+  pre = start;
+  for(;pre->next != NULL; pre = pre->next) {
+    if(pre->next->data == key) {
+      next = pre->next->next;
+      free(pre->next);
+      pre->next = next;
+      next->prev = pre;
+      return start;
+    }
+  }
+}
+
+Node deletePositionDLL(Node start, int pos) {
+  /*
+    delete a Node from Given position.
+  */
+  Node new = malloc(DLL_BLOCK_SIZE), temp1 = start, temp2;
+  for(int i = 1; i < pos; i++) temp1 = temp1->next;
+  temp2 = temp1->next->next;
+  free(temp1->next);
+  temp1->next = temp2;
+  temp2->prev = temp1;
+  return start;
